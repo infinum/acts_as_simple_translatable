64d0331bf853cb99ff37af170b2963bcf8bb11a4
Petar Ćurković <petar.curkovic@infinum.hr>
2019-10-10 13:26:23 +0200

    Update super argument passing

--- lib/acts_as_simple_translatable/class_methods.rb
+++ lib/acts_as_simple_translatable/class_methods.rb
@@ -15,7 +15,7 @@ module ActsAsSimpleTranslatable
       fields.each do |field|
         define_method "#{field}" do
           content = (I18n.locale == I18n.default_locale) ? super() : locale_translations[field]
-          content.present? ? content : super
+          content.present? ? content : super()
         end
 
         define_method "#{field}_original" do
